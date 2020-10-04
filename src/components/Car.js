eac23236d26d2372c3335f5fbae6e9d2bb18c498
michaelscoggins <rhythmghosts@gmail.com>
2020-10-03 23:08:14 -0500

    fully functional

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import cars from '../cars.json'
+
 // import material ui components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
+  const id = props.match.params.id
+  const car = cars.find((car) => id == car.id)
+  //   console.log({ car, id })
+  return <h1>{car.Name}</h1>
 }
 
 export default Car
